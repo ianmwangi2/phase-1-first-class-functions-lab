b6085707ecf09cc18aa57a9a1ea3c5b32430cd1d
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-06-25 10:26:34 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+//return drivers
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+}
+//fare multiplier
+function createFareMultiplier(multiplier) {
+    return function(value) {
+        return value * multiplier;
+    };
+}
+
+// fare doubler
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+//fare trippler
+function fareTripler(fare) {
+    return fare * 3;
+}
